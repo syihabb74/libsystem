d80dd1cd0314b011b19cfa6440b317d24e804d63
syihabb74 <syihabb74@gmail.com>
2025-07-13 22:10:29 +0700

    improve code

--- Library.js
+++ Library.js
@@ -1,90 +1,77 @@
 function Library (name) {
-
     if (!name) {
         throw new Error('Library name is required')
     }
-
     this.name = name
     this.totalBooks = []
     this.totalMembers = []
 }
 
 Library.prototype.addBook = function (book) {
-
     if ( typeof book !== 'object' || !book || Array.isArray(book) ) {
         throw new Error('Book must be an object');
     }
-
     if (!book.id || !book.title || !book.author || !book.isbn ) {
         throw new Error('Book must have id, title, author, and isbn')
     }
-    
     this.totalBooks.forEach( v => {
         if ( v.id === book.id ) {
             throw new Error('Book with this ID already exists');
         }
     })
-    
     this.totalBooks.push(book);
-
     return this.totalBooks.length
-    
 }
 
 Library.prototype.getLibraryStats = function () {
-
     return {
         totalBooks : this.totalBooks.length,
         totalMembers : this.totalMembers.length 
     }
-
 }
 
 Library.prototype.removeBook = function (idBook) {
-    let isBookCanRemove = {
+    let bookIsBorrowed = {
+        isExist : null,
         book : null,
+        isBorrowed : false,
         index : null,
-        bookIsBorrowed : false
     }
-
-    for ( let i = 0 ; i < this.totalBooks.length ; i++  ) {
-        let book = this.totalBooks[i];
-        if ( book.id === idBook ) {
-            if (book.isBorrowing) {
-                isBookCanRemove.bookIsBorrowed = true;
+    this.totalBooks.forEach((v,index) => {
+        if (v.id === idBook) {
+            if ( v.isBorrowing ) {
+                bookIsBorrowed.isExist = true;
+                bookIsBorrowed.book = v;
+                bookIsBorrowed.isBorrowed = true;
             } else {
-                isBookCanRemove.book = book;
-                isBookCanRemove.index = i;
+                bookIsBorrowed.isExist = true;
+                bookIsBorrowed.book = v;
+                bookIsBorrowed.isBorrowed = false;
+                bookIsBorrowed.index = index;
+
             }
-            break;
         }
-    }
-    
-    if (!isBookCanRemove.book) throw new Error('Book not found');
-    if (isBookCanRemove.bookIsBorrowed) throw new Error('Cannot remove borrowed book');
-    let removed = [...this.totalBooks];
-    removed.splice(isBookCanRemove.index,1);
-    this.totalBooks = removed;
-    
-    return isBookCanRemove.book
-
+    })
+    if (!bookIsBorrowed.isExist) throw new Error('Book not found');
+    if (bookIsBorrowed.isBorrowed) throw new Error('Cannot remove borrowed book');
+    let currentLibrary = [...this.totalBooks];
+    currentLibrary.splice(bookIsBorrowed.index, 1);
+    this.totalBooks = currentLibrary;
+    return bookIsBorrowed.book;
 }
 
-Library.prototype.borrowBook =  function(idMember, idBook) {
-    let bookIsBorrowed = this.removeBook(idBook);
-}
+// Library.prototype.borrowBook = function (idMember, idBook) {
+// }
 
 
 
 // id, title, author, isbn
 function Book (id,title,author,isbn) {
-    
     this.id = id
     this.title = title
     this.author = author
     this.isbn = isbn
     this.isBorrowing = ''
-
 }
 
 
@@ -113,60 +100,9 @@ const validMember2 = {
 
 lib1.totalMembers.push(validMember)
 lib1.totalMembers.push(validMember2)
+// lib1.totalBooks[0].isBorrowing = lib1.totalMembers[0].id;
 lib1.totalBooks[0].isBorrowing = lib1.totalMembers[0].id;
-console.log(lib1)
-// console.log(book1)
-// console.log(lib1)
-lib1.borrowBook(lib1.totalMembers[1].id, 'B001');
-// console.log(lib1)
-// console.log(lib1)
-
+console.log('Book status:', lib1.totalBooks[0]);
+lib1.removeBook('B001');
 
 module.exports = Library;
-
-
-
-// // let test = {
-// //     total : [],
-// //     abc : [],
-// //     getStats : function () {
-// //         return {
-// //             total : this.total.length,
-// //             abc: this.total.length
-// //         } 
-// //     }
-// // }
-
-// // console.log(test.getStats().total)
-
-
-// console.log(typeof null)
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let n = 8;
-
-
-
-
-// for ( let i = 1 ; i <= n ; i++ ) {
-//     let symbol= '';
-//     for ( let j = 1 ; j <= n ; j++ ) {
-//         if ( (i * n - n + j) % 2 === 0) {
-//             symbol += '+'
-//         } else {
-//             symbol += '-'
-//         }
-//     }
-//     console.log(symbol)
-// }
